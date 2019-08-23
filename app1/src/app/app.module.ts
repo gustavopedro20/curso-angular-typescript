2e2366f6121b19ee2993cdbf49e2891e36f376de
Gustavo <gustavo.goncalves@fncit.com.br>
2019-08-22 23:35:16 -0300

    att component topo

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent
   ],
   imports: [
     BrowserModule,
