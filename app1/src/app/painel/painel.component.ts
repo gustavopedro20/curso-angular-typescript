f4dc0b851e7c3da5cd8e024c451be047c598f5bc
Gustavo <gustavo.goncalves@fncit.com.br>
2019-08-25 12:14:40 -0300

    até aula 67

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frase-mock'
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  frases: Frase[] = FRASES;
+  instrucao: String = 'Traduza a frase:';
+  resposta: String = '';
+
+  rodada: number = 0;
+  rodadaFrase: Frase;
+
+  progresso: number = 0;
+
+  constructor() {
+    this.atualizaRodada();
+  }
 
   ngOnInit() {
   }
 
+  atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  verificarResposta() {
+    if(this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta');
+      this.rodada++;
+      this.progresso = this.progresso + (100 / this.frases.length);
+      this.atualizaRodada();
+      this.resposta = '';
+    } else {
+      alert('A tradução está errada');
+    }
+  }
+
+  atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+  }
 }
--- app1/src/app/topo/topo.component.ts
+++ app1/src/app/topo/topo.component.ts
@@ -6,5 +6,5 @@ import { Component } from "@angular/core"
     styleUrls: ['./topo.component.css']
 })
 export class TopoComponent {
-
+    public titulo: string = 'Aprendendo Inglês';
 }
